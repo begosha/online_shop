dcace3e33b63cd9891d704260433f26d8a9bfc5c
Gosha <begoshaaaa@gmail.com>
2021-04-02 14:58:30 +0600

    path to index view is made

---
+++ source/shop/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
+    # path('<int:pk>/', ProjectView.as_view(), name='project'),
+    # path('<int:pk>/task', TaskView.as_view(), name='task'),
+    # path('add/', ProjectCreate.as_view(), name='project-add'),
+    # path('<int:pk>/update/project', ProjectUpdateView.as_view(), name='project-update'),
+    # path('<int:pk>/add/', TaskCreate.as_view(), name='task_add'),
+    # path('<int:pk>/update', TaskUpdateView.as_view(), name='task-update'),
+    # path('<int:pk>/delete', TaskDeleteView.as_view(), name='task-delete'),
+    # path('<int:pk>/delete/project', ProjectDeleteView.as_view(), name='project-delete'),
+
+]
