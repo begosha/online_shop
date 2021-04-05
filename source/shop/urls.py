948d06406754032545258a300f6621f06888d1a9
Gosha <begoshaaaa@gmail.com>
2021-04-05 15:12:29 +0600

    paths to delete and update view are fixed

--- source/shop/urls.py
+++ source/shop/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import (IndexView, ProductView, ProductDeleteView, ProductUpdateView)
+from .views import (IndexView, ProductView, ProductDeleteView, ProductUpdateView, ProductCreateView)
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     # path('<int:pk>/', ProjectView.as_view(), name='project'),
     path('<int:pk>/product', ProductView.as_view(), name='product'),
-    # path('add/', ProjectCreate.as_view(), name='project-add'),
+    path('add/', ProductCreateView.as_view(), name='product-add'),
     # path('<int:pk>/update/project', ProjectUpdateView.as_view(), name='project-update'),
     # path('<int:pk>/add/', TaskCreate.as_view(), name='task_add'),
     path('<int:pk>/update', ProductUpdateView.as_view(), name='product-update'),
